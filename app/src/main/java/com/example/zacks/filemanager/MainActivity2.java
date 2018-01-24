bd85a9e197081b46f9ab97b116a303bc2d2ea41e
zacks666 <763652797@qq.com>
2018-01-24 17:57:57 +0800

    filemanager

--- app/src/main/java/com/example/zacks/filemanager/MainActivity2.java
+++ app/src/main/java/com/example/zacks/filemanager/MainActivity2.java
@@ -16,7 +16,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -39,6 +38,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 
 
